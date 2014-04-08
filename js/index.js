d654c088d32be2637416cbefd9f3f25fa63520bc
Pedro Torres <phtg@cin.ufpe.br>
2014-04-08 12:04:40 -0300

    Bug fix

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var typingTimer;
-var interval = 1000;
+var interval = 500;
 $("#busca").keyup(function() {
 	clearTimeout(typingTimer);
 	typingTimer = setTimeout(suggestMovies, interval);
@@ -12,13 +12,14 @@ $("#busca").keydown(function() {
 function suggestMovies () {
 	var text = $("#busca").val();
 	
+	$("#sugestoes").empty();
 	$.ajax({
 		url: "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=qv67jaxpgqksy7cfvmcryhau&q=" + text,
 
 		dataType: 'jsonp',
 
 		success: function(suggestions) {
-			for (var i = 0; i < suggestions.movies.length; i++) {
+			for (var i = 0; i < suggestions.movies.length && i < 4; i++) {
 				$("#sugestoes").append("<a href='#' onclick='addMovie(this)' class='sugestao'><p id=" + i + ">" + suggestions.movies[i].title + "</p></a>");
 			};
 		}
@@ -34,7 +35,6 @@ function addMovie(me) {
 		dataType: 'jsonp',
 
 		success: function(movies) {
-			console.log(movies.movies[0]);
 			$("#filmes").append("<img src=" + movies.movies[0].posters.original + " class='filme'>");
 		}
 	});
