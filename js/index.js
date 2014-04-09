d803b2bdb0363142306329af105ca6c63ee7d9a2
Pedro Torres <phtg@cin.ufpe.br>
2014-04-08 21:14:36 -0300

    Contador de tempo funcionando

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+var totalTime = 0;
+
 var typingTimer;
 var interval = 500;
 $("#busca").keyup(function() {
@@ -28,14 +30,33 @@ function suggestMovies () {
 
 function addMovie(me) {
 	var movieName = $(me).first("p").text();
-	
+	var position = me.getElementsByTagName('p')[0].id;
+
+	var runtime = 0;
 	$.ajax({
 		url: "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=qv67jaxpgqksy7cfvmcryhau&q=" + movieName,
 
 		dataType: 'jsonp',
 
 		success: function(movies) {
-			$("#filmes").append("<img src=" + movies.movies[0].posters.original + " class='filme'>");
+			$("#filmes").append("<img src=" + movies.movies[position].posters.original + " class='filme'>");
+			runtime = movies.movies[position].runtime;
+
+			calculateTime(runtime);
 		}
 	});
+};
+
+function calculateTime(runtime) {
+	totalTime += runtime;
+
+	var hours = Math.floor(totalTime / 60);
+	var days = Math.floor(hours / 24);
+
+	hours = hours % 24;
+	var minutes = totalTime % 60;
+
+	$("#dias").text(days);
+	$("#horas").text(hours);
+	$("#minutos").text(minutes);
 };
