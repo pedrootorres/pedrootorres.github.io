215a61fd02a002d2a11f3fe768f6ef64e74ba4d4
pedrootorres <phtg@cin.ufpe.br>
2014-08-12 01:24:22 +0100

    Colocando balaozinho quando clica nos pins

--- pinmap/js/index.js
+++ pinmap/js/index.js
@@ -22,8 +22,8 @@ function initialize() {
 }
 
 var places = [
-  {"name": "Liverpool", "lat": 53.40837, "lng": -2.99157},
-  {"name": "Dundee", "lat": 56.46202, "lng": -2.97072}
+  {"name": "Liverpool", "lat": 53.40837, "lng": -2.99157, "things": "<div id='content'><div id='siteNotice'></div><h1 id='firstHeading' class='firstHeading'>Liverpool</h1><div id='bodyContent'><p>teste2</p></div></div>"},
+  {"name": "Dundee", "lat": 56.46202, "lng": -2.97072, "things": "<div id='content'><div id='siteNotice'></div><h1 id='firstHeading' class='firstHeading'>Dundee</h1><div id='bodyContent'><p>teste</p></div></div>"}
 ];
 
 function loadPlaces(map) {
@@ -31,8 +31,18 @@ function loadPlaces(map) {
     var placesMarkers = new google.maps.Marker({
       position: new google.maps.LatLng(places[i].lat, places[i].lng),
       map: map,
-      title: places[i].name
+      title: places[i].name,
+      html: places[i].things
     });
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: "wait for it"
+    });
+
+    google.maps.event.addListener(placesMarkers, 'click', function() {
+      infoWindow.setContent(this.html);
+      infoWindow.open(map, this);
+    })
   }
 }
 
