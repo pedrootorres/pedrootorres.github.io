7363839a7217233970ad61a56cee7baa5cd04db6
pedrootorres <phtg@cin.ufpe.br>
2015-08-07 11:59:50 +0100

    Adding wish list menu

--- pinmap/js/index.js
+++ pinmap/js/index.js
@@ -1,11 +1,3 @@
-$(".panel-heading").click(function() {
-  if($("#leftMenu").css("margin-left") != "0px") {
-    $("#leftMenu").animate({"margin-left": "0px"}, 500);
-  } else {
-    $("#leftMenu").animate({"margin-left": "-200px"}, 500);
-  }
-});
-
 function initialize() {
   var ormskirk = new google.maps.LatLng(53.56894, -2.88506);
   var originalPosition = new google.maps.LatLng(30, 0)
@@ -95,64 +87,97 @@ function initialize() {
     {"name": "Bolton", "country": "England", "lat": 53.57686, "lng": -2.42822, "info": "<div class='caption'><h3>Bolton</h3><p class='text-justify'>Town in Greater Manchester.</p><p class='text-justify'><strong>Cool things I did here:</strong> Met an incredible family and stayed in an English Home for first time</p></div>"},
   ];
 
-  var friends =[
-    {"name": "Recife", "country": "Brazil", "lat": -8.04755, "lng": -34.87696},
-    {"name": "Fortaleza", "country": "Brazil", "lat": -3.73186, "lng": -38.52667},
-    {"name": "São Paulo", "country": "Brazil", "lat": -23.55052, "lng": -46.63331},
-    {"name": "Salvador", "country": "Brazil", "lat": -12.97223, "lng": -38.50142},
-    {"name": "Belo Horizonte", "country": "Brazil", "lat": -19.92452, "lng": -43.93527},
-    {"name": "Brasília", "country": "Brazil", "lat": -15.79415, "lng": -47.88255},
-    {"name": "Araranguá", "country": "Brazil", "lat": -28.93612, "lng": -49.49230},
-    {"name": "Rajkot", "country": "India", "lat": 22.30389, "lng": 70.80216},
-    {"name": "Chongqing", "country": "China", "lat": 29.56301, "lng": 106.55156},
-    {"name": "Beijing", "country": "China", "lat": 39.90403, "lng": 116.40753},
-    {"name": "Shenzhen", "country": "China", "lat": 22.54310, "lng": 114.05787},
-    {"name": "Lusaka", "country": "Zambia", "lat": -15.41667, "lng": 28.28333},
-    {"name": "Hefei", "country": "China", "lat": 31.82059, "lng": 117.22724},
-    {"name": "Macau", "country": "China", "lat": 22.19874, "lng": 113.54387},
-    {"name": "Chandigarh", "country": "India", "lat": 30.73331, "lng": 76.77942},
-    {"name": "Xichang", "country": "China", "lat": 27.89450, "lng": 102.26445},
-    {"name": "Zhongshan", "country": "China", "lat": 22.51700, "lng": 113.39272},
-    {"name": "York", "country": "England", "lat": 53.95997, "lng": -1.08730},
-    {"name": "Santa Maria Capua Vetere", "country": "Italy", "lat": 41.08209, "lng": 14.25419},
-    {"name": "Jeonju", "country": "Korea", "lat": 35.82422, "lng": 127.14795},
-    {"name": "Iksan", "country": "Korea", "lat": 35.94829, "lng": 126.95760},
-    {"name": "Daejeon", "country": "Korea", "lat": 36.35041, "lng": 127.38455},
-    {"name": "Oldenburg", "country": "Germany", "lat": 53.14345, "lng": 8.21455},
-    {"name": "London", "country": "England", "lat": 51.50735, "lng": -0.12776},
-    {"name": "Manchester", "country": "England", "lat": 53.47932, "lng": -2.24849},
-    {"name": "Heywood", "country": "England", "lat": 53.59263, "lng": -2.22565},
-    {"name": "Huesca", "country": "Spain", "lat": 42.13184, "lng": -0.40781},
-    {"name": "Zaragoza", "country": "Spain", "lat": 41.64882, "lng": -0.88909},
-    {"name": "Mülheim an der Ruhr", "country": "Germany", "lat": 51.41857, "lng": 6.88452},
-    {"name": "Freehold", "country": "USA", "lat": 40.22438, "lng": -74.29905},
-    {"name": "Oklahoma", "country": "USA", "lat": 35.46756, "lng": -97.51643},
-    {"name": "Caen", "country": "France", "lat": 49.18286, "lng": -0.37068},
-    {"name": "Guadalajara", "country": "Mexico", "lat": 20.65970, "lng": -103.34961},
-    {"name": "Amfreville-Sur-Iton", "country": "France", "lat": 49.14717, "lng": 1.14912},
-    {"name": "Hong Kong", "country": "China", "lat": 22.39865, "lng": 114.09465},
-    {"name": "Walgrave", "country": "England", "lat": 52.34147, "lng": -0.82277},
-    {"name": "Bolton", "country": "England", "lat": 53.57686, "lng": -2.42822},
+  // var friends =[
+  //   {"name": "Recife", "country": "Brazil", "lat": -8.04755, "lng": -34.87696},
+  //   {"name": "Fortaleza", "country": "Brazil", "lat": -3.73186, "lng": -38.52667},
+  //   {"name": "São Paulo", "country": "Brazil", "lat": -23.55052, "lng": -46.63331},
+  //   {"name": "Salvador", "country": "Brazil", "lat": -12.97223, "lng": -38.50142},
+  //   {"name": "Belo Horizonte", "country": "Brazil", "lat": -19.92452, "lng": -43.93527},
+  //   {"name": "Brasília", "country": "Brazil", "lat": -15.79415, "lng": -47.88255},
+  //   {"name": "Araranguá", "country": "Brazil", "lat": -28.93612, "lng": -49.49230},
+  //   {"name": "Rajkot", "country": "India", "lat": 22.30389, "lng": 70.80216},
+  //   {"name": "Chongqing", "country": "China", "lat": 29.56301, "lng": 106.55156},
+  //   {"name": "Beijing", "country": "China", "lat": 39.90403, "lng": 116.40753},
+  //   {"name": "Shenzhen", "country": "China", "lat": 22.54310, "lng": 114.05787},
+  //   {"name": "Lusaka", "country": "Zambia", "lat": -15.41667, "lng": 28.28333},
+  //   {"name": "Hefei", "country": "China", "lat": 31.82059, "lng": 117.22724},
+  //   {"name": "Macau", "country": "China", "lat": 22.19874, "lng": 113.54387},
+  //   {"name": "Chandigarh", "country": "India", "lat": 30.73331, "lng": 76.77942},
+  //   {"name": "Xichang", "country": "China", "lat": 27.89450, "lng": 102.26445},
+  //   {"name": "Zhongshan", "country": "China", "lat": 22.51700, "lng": 113.39272},
+  //   {"name": "York", "country": "England", "lat": 53.95997, "lng": -1.08730},
+  //   {"name": "Santa Maria Capua Vetere", "country": "Italy", "lat": 41.08209, "lng": 14.25419},
+  //   {"name": "Jeonju", "country": "Korea", "lat": 35.82422, "lng": 127.14795},
+  //   {"name": "Iksan", "country": "Korea", "lat": 35.94829, "lng": 126.95760},
+  //   {"name": "Daejeon", "country": "Korea", "lat": 36.35041, "lng": 127.38455},
+  //   {"name": "Oldenburg", "country": "Germany", "lat": 53.14345, "lng": 8.21455},
+  //   {"name": "London", "country": "England", "lat": 51.50735, "lng": -0.12776},
+  //   {"name": "Manchester", "country": "England", "lat": 53.47932, "lng": -2.24849},
+  //   {"name": "Heywood", "country": "England", "lat": 53.59263, "lng": -2.22565},
+  //   {"name": "Huesca", "country": "Spain", "lat": 42.13184, "lng": -0.40781},
+  //   {"name": "Zaragoza", "country": "Spain", "lat": 41.64882, "lng": -0.88909},
+  //   {"name": "Mülheim an der Ruhr", "country": "Germany", "lat": 51.41857, "lng": 6.88452},
+  //   {"name": "Freehold", "country": "USA", "lat": 40.22438, "lng": -74.29905},
+  //   {"name": "Oklahoma", "country": "USA", "lat": 35.46756, "lng": -97.51643},
+  //   {"name": "Caen", "country": "France", "lat": 49.18286, "lng": -0.37068},
+  //   {"name": "Guadalajara", "country": "Mexico", "lat": 20.65970, "lng": -103.34961},
+  //   {"name": "Amfreville-Sur-Iton", "country": "France", "lat": 49.14717, "lng": 1.14912},
+  //   {"name": "Hong Kong", "country": "China", "lat": 22.39865, "lng": 114.09465},
+  //   {"name": "Walgrave", "country": "England", "lat": 52.34147, "lng": -0.82277},
+  //   {"name": "Bolton", "country": "England", "lat": 53.57686, "lng": -2.42822},
+  // ];
+
+  var wishList = [
+    {"name": "Istanbul", "country": "Turkey", "lat": 41.00824, "lng": 28.97836}
   ];
 
   loadPlaces(map, places);
+  // loadWishList(map, wishList);
   // loadFriends(map, friends);
 
   var friendsMarker = [];
 
-  $("#friendsCheckbox").change(function() {
-    if(this.checked) {
-      loadFriends(map, friends, friendsMarker);
-    } else {
-      for (var i = 0; i < friendsMarker.length; i++) {
-        friendsMarker[i].setMap(null);
-      };
+  // $("#friendsCheckbox").change(function() {
+  //   if(this.checked) {
+  //     loadFriends(map, friends, friendsMarker);
+  //   } else {
+  //     for (var i = 0; i < friendsMarker.length; i++) {
+  //       friendsMarker[i].setMap(null);
+  //     };
 
-      $("#accordion2").empty();
-    }
-  })
+  //     $("#accordion2").empty();
+  //   }
+  // })
 
   // $("#titlePlaces").text("Places I've been (" + (places.length+1) + ")");
+
+  $("#headingPlaces").click(function() {
+    if($("#leftMenu").css("margin-left") != "0px") {
+      $("#leftMenu").animate({"margin-left": "0px"}, 200);
+      $("#leftMenu").css("z-index", "99");
+    } else {
+      $("#leftMenu").animate({"margin-left": "-200px"}, 200);
+      $("#leftMenu").css("z-index", "2");
+    }
+  });
+
+  $("#headingWishList").click(function() {
+    if($("#leftMenuWishList").css("margin-left") != "0px") {
+      $("#leftMenuWishList").animate({"margin-left": "0px"}, 200);
+      $("#leftMenuWishList").css("z-index", "99");
+      $("#leftMenuWishList").animate({"margin-top": "20px"}, 200);
+      loadWishList(map, wishList, friendsMarker);
+    } else {
+      $("#leftMenuWishList").animate({"margin-top": "60px"}, 200);
+      $("#leftMenuWishList").animate({"margin-left": "-200px"}, 200);
+      $("#leftMenuWishList").css("z-index", "1");
+      for (var i = 0; i < wishList.length; i++) {
+        wishListMarkers[i].setMap(null);
+      };
+
+      $("#accordionWishList").empty();
+    }
+  });
 }
 
 function loadPlaces(map, places) {
@@ -187,28 +212,50 @@ function loadPlaces(map, places) {
   $("#titlePlaces").text("Places I've been (" + ($(".cities").length) + ")");
 }
 
-function loadFriends(map, friends, friendsMarker) {
-  var colapso = 1;
-  var icon = 'img/friends_icon.png';
+var wishListMarkers = [];
+function loadWishList(map, wishList) {
+  var collapseWish = 1;
 
-  for(var i = 0; i < friends.length; i++) {
+  for(var i = 0; i < wishList.length; i++) {
     var placesMarkers = new google.maps.Marker({
-      position: new google.maps.LatLng(friends[i].lat, friends[i].lng),
+      position: new google.maps.LatLng(wishList[i].lat, wishList[i].lng),
       map: map,
-      icon: icon,
-      title: friends[i].name
+      title: wishList[i].name
     });
 
-    friendsMarker.push(placesMarkers);
+    wishListMarkers.push(placesMarkers);
 
-    if($("#"+friends[i].country+"2").length == 0 ){
-      $("#accordion2").append('<a data-toggle="collapse" data-parent="#accordion2" href="#colapso'+colapso+'"><h4 id="'+friends[i].country+'2">'+friends[i].country+'</h4></a><div id="colapso'+colapso+'" class="panel-collapse collapse"><div class="cities" id="friends'+friends[i].country+'"><p>'+friends[i].name+'</p></div></div>');
-      colapso++;
+    if($("#"+wishList[i].country.replace(" ", "_") + "2").length == 0 ){
+      $("#accordionWishList").append('<a data-toggle="collapse" data-parent="#accordionWishList" href="#collapseWish'+collapseWish+'"><h4 id="'+wishList[i].country.replace(" ", "_")+'">'+wishList[i].country+'</h4></a><div id="collapseWish'+collapseWish+'" class="panel-collapse collapse"><div class="cities" id="citiesWish'+wishList[i].country.replace(" ", "_")+'"><p>'+wishList[i].name+'</p></div></div>');
+      collapseWish++;
     } else {
-      $("#friends"+friends[i].country).append('<p>'+friends[i].name+'</p>');
+      $("#citiesWish"+wishList[i].country.replace(" ", "_")).append('<p>'+wishList[i].name+'</p>');
     }
   }
 }
+
+// function loadFriends(map, friends, friendsMarker) {
+//   var colapso = 1;
+//   var icon = 'img/friends_icon.png';
+
+//   for(var i = 0; i < friends.length; i++) {
+//     var placesMarkers = new google.maps.Marker({
+//       position: new google.maps.LatLng(friends[i].lat, friends[i].lng),
+//       map: map,
+//       icon: icon,
+//       title: friends[i].name
+//     });
+
+//     friendsMarker.push(placesMarkers);
+
+//     if($("#"+friends[i].country+"2").length == 0 ){
+//       $("#accordion2").append('<a data-toggle="collapse" data-parent="#accordion2" href="#colapso'+colapso+'"><h4 id="'+friends[i].country+'2">'+friends[i].country+'</h4></a><div id="colapso'+colapso+'" class="panel-collapse collapse"><div class="cities" id="friends'+friends[i].country+'"><p>'+friends[i].name+'</p></div></div>');
+//       colapso++;
+//     } else {
+//       $("#friends"+friends[i].country).append('<p>'+friends[i].name+'</p>');
+//     }
+//   }
+// }
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
